2d1b5ea821dcc67f53a38f5e779d6a7894b54df6
ShankarChavan <ShankarChavan@users.noreply.github.com>
2023-01-20 12:55:58 +0000

    update with mutable

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -2,11 +2,9 @@
 const STARTING_MISSILES:i32 =8;
 const READY_AMOUNT:i32 =1;
 fn main() {
-
-    let _da=123;
-    let (missiles,ready)=(STARTING_MISSILES,READY_AMOUNT);
+    let (mut missiles,ready)=(STARTING_MISSILES,READY_AMOUNT);
 
     println!("Firing {} of my {} missiles...", ready, missiles);
-
+    missiles=missiles-ready;
     print!("{} missiles left",missiles-ready);
 }
